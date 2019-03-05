ef26aba28ffc829515750e3931b02a5a288608f0
Julia Gottschalk <giulia.gottschalk@gmail.com>
2019-03-05 19:44:24 +0100

    Updates

---
+++ precache-manifest.e6b43a88bd0fa598b4e8c88acff22240.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "940558acfbb5a5d3faa7",
+    "url": "/GnomesApp/static/css/main.c2183e3c.chunk.css"
+  },
+  {
+    "revision": "940558acfbb5a5d3faa7",
+    "url": "/GnomesApp/static/js/main.940558ac.chunk.js"
+  },
+  {
+    "revision": "78633f32f20bad7b9c5e",
+    "url": "/GnomesApp/static/css/1.eeeb8ee6.chunk.css"
+  },
+  {
+    "revision": "78633f32f20bad7b9c5e",
+    "url": "/GnomesApp/static/js/1.78633f32.chunk.js"
+  },
+  {
+    "revision": "cf0a64898d933a4d70a3",
+    "url": "/GnomesApp/static/js/runtime~main.cf0a6489.js"
+  },
+  {
+    "revision": "9cbe3db87f1060e6e9bf55f5fe16ec8a",
+    "url": "/GnomesApp/static/media/gnomes-bg.9cbe3db8.jpg"
+  },
+  {
+    "revision": "e4ef80ab75beb23271dec4c4fb7363d9",
+    "url": "/GnomesApp/index.html"
+  }
+];
